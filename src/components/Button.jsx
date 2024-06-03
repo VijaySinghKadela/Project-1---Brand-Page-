56ccd099443159e489de71b56540ac470ebf7fb0
VinaySinghKadela <132544545+VijaySinghKadela@users.noreply.github.com>
2024-06-03 21:40:39 +0530

    added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
+import React from 'react'
 
-const button = () => {
+const Button = () => {
   return (
-    <div>button</div>
+    <div className="flex gap-8 mt-10">
+      <button className='px-3 py-1 bg-red-700 text-white rounded-sm font-semibold '>Shop Now</button>
+      <button className='px-3 py-1 bg-white- text-gray-600 border-2 border-gray-500 rounded-sm font-semibold'>Category</button>
+    </div>
   )
 }
 
-export default button
+export default Button
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,28 @@
 import Button from './Button';
 const content = () => {
   return (
-    <div className='mt-20 px-56 max-sm:mt-10 max-sm:bg-red-400'>
-      <div className="left-container flex flex-col w-1/2  max-h-screen-xl  ">
-        <h1 className='font-extrabold text-8xl  '>YOUR FEET DESERVE THE BEST</h1>
-        <p className='mt-16'>
+    <div className='lg:mt-20 lg:px-56 sm:mt-16  max-sm:mt-16 sm:w-full sm:flex sm:justify-center   sm:h-screen max-sm:w-full max-sm:flex max-sm:justify-center   max-sm:h-screen'>
+      <div className="left-container flex flex-col lg:w-full max-sm:w-full sm:px-24 max-sm:px-12  max-h-screen-xl  ">
+        <div className='headling_img flex gap-34 flex-row '>
+        <h1 className='font-extrabold lg:mx-36 lg:text-8xl sm:text-6xl md:text-6xl max-sm:text-5xl max-md:text-4xl  '>YOUR FEET DESERVE THE BEST</h1>
+        
+        </div>
+        <p className='lg:mt-16 sm:mt-8 max-sm:mt-12 max-sm:text-sm lg:text-xl lg:mx-24 '>
           YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
           SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
           SHOES.
         </p>
-        {/* <Button/>
-        <p>Also Available On</p>
+     
+        <Button/>
+
+
+        {/* <p>Also Available On</p>
         <div className='icons-img'>
             <img src="../public/Image/flipkart.png" alt="" />
             <img src="../public/Image/amazon_logo.png" alt="" />
+        </div>  */}
+        {/* <div className='img '>
+            <img className='size-.0 lg:block max-sm:hidden max-md:hidden' src="./public/Image/shoe_image.png" alt="" />
         </div> */}
       </div>
     </div>
